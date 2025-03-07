924fe80cc3ff547cf435120e54b452a4452da65a
prajwalthapaa <121283639+prajwalthapaa@users.noreply.github.com>
2025-03-07 10:36:00 +0545

    Delete flutter_hotel_booking-main directory

--- flutter_hotel_booking-main/lib/core/util/int.dart
+++
@@ -1,4 +0,0 @@
-extension IntDurationX on int {
-  Duration get miliseconds => Duration(milliseconds: this);
-  Duration get seconds => Duration(seconds: this);
-}
--- flutter_hotel_booking-main/lib/domain/repositories/AuthRepository.dart
+++
@@ -1,6 +0,0 @@
-abstract class AuthRepository {
-  Future<bool> createAccount(String name, String email, String password);
-  Future<bool> login(String email, String password);
-  Future<bool> isLoggedIn();
-  Future<void> logout();
-}
